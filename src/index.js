a40b0dc7c11ab4ba100aadd100d9784bea37b919
ththanh <hoaithanhtrinh2002@gmail.com>
2023-01-26 16:08:07 +0700

    login and register

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import {Routes, unstable_HistoryRouter as HistoryRouter, Route, Navigate} from '
 import { createBrowserHistory } from 'history';
 import {store} from './redux/configureStore'
 import HomeTemplate from './Templates/HomeTemplate/HomeTemplate';
-
+import UserHome from './Pages/User/UserHome/UserHome';
+import UserLogin from './Pages/User/UserLogin/UserLogin';
+import "./assets/scss/style.scss";
+import UserRegister from './Pages/User/UserRegister/UserRegister';
+import UserProfile from './Pages/User/UserProfile/UserProfile';
 export const history = createBrowserHistory()
 
 
@@ -15,6 +19,10 @@ root.render(
     <HistoryRouter history={history}>
         <Routes >
             <Route path='' element={<HomeTemplate />}>
+                <Route index element={<UserHome/>}></Route>
+                <Route path='registerUser' element={<UserRegister/>}></Route>
+                <Route path='loginUser' element={<UserLogin/>}></Route>
+                <Route path='profileUser' element={<UserProfile/>}></Route>
                 <Route path='*' element={<Navigate to='/'/>}></Route>
             </Route>
         </Routes>
